58eb1ca919068975ee0ba75890eebf8136bcb306
palinkas-jo-reggelt <52140617+palinkas-jo-reggelt@users.noreply.github.com>
2022-05-03 07:44:22 -0400

    Update foot.php

--- www/foot.php
+++ www/foot.php
@@ -3,7 +3,7 @@
 	<div class="footer">
 		Copyright 2022 Pálinkás Jó Reggelt
 <?php 
-(int)$versionGitHub = file_get_contents('https://raw.githubusercontent.com/palinkas-jo-reggelt/hMailServer_DynRBLWL/main/VERSION');
+(int)$versionGitHub = file_get_contents('https://raw.githubusercontent.com/palinkas-jo-reggelt/hMailServer_pURIBL/main/VERSION');
 (int)$versionLocal = file_get_contents('VERSION');
 if ($versionLocal < $versionGitHub) {
 	echo "<br><br>Upgrade to version ".$versionGitHub." available at <a href='https://github.com/palinkas-jo-reggelt/hMailServer-IDS-Viewer'>GitHub</a>";
